61f556070084bfa3256411a722631b0a29e1ac07
Jafar Qaddoumi <jafarkhq@gmail.com>
2015-10-14 17:02:31 +0200

    add base singup handler

--- src/main/java/com/epam/socode/controller/AuthController.java
+++ src/main/java/com/epam/socode/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.epam.socode.controller;
 
+import com.epam.socode.request.Signup;
+import com.epam.socode.response.Response;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(BaseController.MAPPING_API_AUTH)
 class AuthController implements BaseController {
+
+    @RequestMapping(value = "/test",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.GET)
+    public Response handleTest() {
+        return Response.newSuccessResponse();
+    }
+
+    @RequestMapping(value = MAPPING_SIGNUP,
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Response handleSignup(@RequestBody Signup signup) {
+
+        return Response.newSuccessResponse(signup);
+    }
 }
--- src/main/java/com/epam/socode/controller/BaseController.java
+++ src/main/java/com/epam/socode/controller/BaseController.java
@@ -7,4 +7,8 @@ interface BaseController {
     // Mapping
     String MAPPING_API = "/api";
     String MAPPING_API_AUTH = MAPPING_API + "/auth";
+
+    String MAPPING_SIGNUP = "/signup";
+
+    //String PARAM_ = "/signup";
 }
