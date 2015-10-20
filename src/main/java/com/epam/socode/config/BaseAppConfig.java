b955d79a6363288c643872e166eabb04657786a8
Jafar Qaddoumi <jafarkhq@gmail.com>
2015-10-20 10:56:58 +0200

    test for java cofig

--- src/main/java/com/epam/socode/config/BaseAppConfig.java
+++ src/main/java/com/epam/socode/config/BaseAppConfig.java
@@ -1,13 +1,26 @@
 package com.epam.socode.config;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hibernate.ogm.cfg.OgmConfiguration;
 import org.hibernate.ogm.cfg.OgmProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceProviderResolverHolder;
+import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Common App Configuration for All environments (Profiles)
@@ -39,16 +52,32 @@ class BaseAppConfig {
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
-        final Map<String, String> props = new HashMap<>(7);
+        final Properties props = new Properties();
+        props.setProperty(OgmProperties.DATASTORE_PROVIDER, provider);
+        props.setProperty(OgmProperties.HOST, host);
+        props.setProperty(OgmProperties.DATABASE, database);
+        props.setProperty(OgmProperties.USERNAME, username);
+        props.setProperty(OgmProperties.PASSWORD, password);
+        props.setProperty(OgmProperties.CREATE_DATABASE, createDatabase);
 
-        props.put(OgmProperties.DATASTORE_PROVIDER, provider);
-        props.put(OgmProperties.HOST, host);
-        props.put(OgmProperties.DATABASE, database);
-        props.put(OgmProperties.USERNAME, username);
-        props.put(OgmProperties.PASSWORD, password);
-        props.put(OgmProperties.CREATE_DATABASE, createDatabase);
-
-        return Persistence.createEntityManagerFactory(persistenceUnit, props);
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan("com.epam.socode.domain");
+        factoryBean.setJpaProperties(props);
+        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
     }
 
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mongodb.Mongo");
+        dataSource.setUrl(database);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
