4d9d0c6e95c587d5a8dfdefd9e1dc8a4977021d3
Jafar Qaddoumi <jafarkhq@gmail.com>
2015-11-06 14:32:54 +0100

    group memper controller

--- src/main/java/com/epam/socode/controller/BaseController.java
+++ src/main/java/com/epam/socode/controller/BaseController.java
@@ -9,6 +9,7 @@ interface BaseController {
     String MAPPING_API_AUTH = MAPPING_API + "/auth";
     String MAPPING_API_PROFILE = MAPPING_API + "/profile";
     String MAPPING_API_GROUP = MAPPING_API + "/group";
+    String MAPPING_API_GROUP_MEMBERS = MAPPING_API_GROUP + "/members";
 
     String MAPPING_SIGNUP = "/signup";
     String MAPPING_VERIFY = "/verify";
@@ -16,6 +17,7 @@ interface BaseController {
     String MAPPING_CREATE = "/create";
     String MAPPING_LOGOUT = "/logout";
     String MAPPING_UPDATE = "/update";
+    String MAPPING_ADD = "/add";
 
     // String PARAM_ = "/signup";
 }
