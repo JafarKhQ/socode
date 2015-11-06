f0993562d99c2b36842862985b7f3feeae79ed8b
Jafar Qaddoumi <jafarkhq@gmail.com>
2015-11-06 11:27:30 +0100

    fix names

--- src/main/java/com/epam/socode/controller/BaseController.java
+++ src/main/java/com/epam/socode/controller/BaseController.java
@@ -8,13 +8,12 @@ interface BaseController {
     String MAPPING_API = "/api";
     String MAPPING_API_AUTH = MAPPING_API + "/auth";
     String MAPPING_API_PROFILE = MAPPING_API + "/profile";
-    
     String MAPPING_API_GROUP = MAPPING_API + "/group";
-    String MAPPING_API_GROUP_CREATE = "/create";
 
     String MAPPING_SIGNUP = "/signup";
     String MAPPING_VERIFY = "/verify";
     String MAPPING_LOGIN = "/login";
+    String MAPPING_CREATE = "/create";
     String MAPPING_LOGOUT = "/logout";
     String MAPPING_UPDATE = "/update";
 
--- src/main/java/com/epam/socode/controller/GroupController.java
+++ src/main/java/com/epam/socode/controller/GroupController.java
@@ -1,18 +1,17 @@
 package com.epam.socode.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.epam.socode.domain.GroupData;
 import com.epam.socode.domain.WorkGroup;
 import com.epam.socode.request.GroupCreate;
 import com.epam.socode.response.Response;
 import com.epam.socode.service.AuthenticationService;
 import com.epam.socode.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(BaseController.MAPPING_API_GROUP)
@@ -24,15 +23,15 @@ class GroupController implements BaseController {
     private AuthenticationService authenticationService;
 
 
-    @RequestMapping(value = BaseController.MAPPING_API_GROUP_CREATE,
+    @RequestMapping(value = BaseController.MAPPING_CREATE,
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Response handleProfileUpdate(@RequestBody GroupCreate groupCreate) {
+    public Response handleGroupCreate(@RequestBody GroupCreate groupCreate) {
         authenticationService.validateToken(groupCreate.getToken());
-        String profileId  = authenticationService.findProfileIdByToken(groupCreate.getToken());
-        WorkGroup workGroup = groupService.addGroup(new GroupData(groupCreate.getGroupName(),profileId));
-        
+        String profileId = authenticationService.findProfileIdByToken(groupCreate.getToken());
+        WorkGroup workGroup = groupService.addGroup(new GroupData(groupCreate.getGroupName(), profileId));
+
         return Response.newSuccessResponse(workGroup);
     }
 }
