f976e37e7634b1c0a6a9f74330e630c213f35c28
Jafar Qaddoumi <jafarkhq@gmail.com>
2015-11-09 10:57:38 +0100

    fix test failes randomly !!

--- src/main/java/com/epam/socode/event/OnRegistrationCompleteEvent.java
+++ src/main/java/com/epam/socode/event/OnRegistrationCompleteEvent.java
@@ -1,6 +1,7 @@
 package com.epam.socode.event;
 
 import com.epam.socode.domain.Profile;
+import com.epam.socode.domain.VerificationKey;
 import org.springframework.context.ApplicationEvent;
 
 /**
@@ -9,12 +10,19 @@ import org.springframework.context.ApplicationEvent;
 public class OnRegistrationCompleteEvent extends ApplicationEvent {
     private static final long serialVersionUID = 6963784933841027704L;
 
-    public OnRegistrationCompleteEvent(Profile profile) {
+    private VerificationKey verificationKey;
+
+    public OnRegistrationCompleteEvent(Profile profile, VerificationKey verificationKey) {
         super(profile);
+        this.verificationKey = verificationKey;
     }
 
     @Override
     public Profile getSource() {
         return (Profile) super.getSource();
+    }
+
+    public VerificationKey getVerificationKey() {
+        return verificationKey;
     }
 }
